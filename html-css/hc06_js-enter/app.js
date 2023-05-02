894a45f64f81707a822ff923274bfe0e36bef8ba
galiphasandugan <galiphasandugan@gmail.com>
2023-05-02 23:03:14 +0300

    lsson start

---
+++ html-css/hc06_js-enter/app.js
@@ -0,0 +1,29 @@
+// console.log("ben geldim");
+
+// alert("bu bier alaerttir");
+
+// karar = confirm("emin misiniz E/H");
+// console.log("alerten sonrki komut");
+
+// console.warn("Bu bir uyarıdır");
+
+// console.error("eywah hata oluştu");
+
+// !***************************** CONST*****
+console.log("APP JS RUNNİNG");
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayı1 = "3";
+console.log(typeof sayı1);
+
+// const deneme ;
+// console.log(deneme);
+
+const number1 = 5;
+number1 = 7;
+// ! const ile tanımlanan değişkene başlangıç değişkeni verilmelidir.
+
+const varMı = true;
+const isOpen = false;
