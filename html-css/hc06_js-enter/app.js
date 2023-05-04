f6f8625629b011a5e04e82620f83927ea5d5a18d
galiphasandugan <galiphasandugan@gmail.com>
2023-05-04 20:07:15 +0300

    commit

--- html-css/hc06_js-enter/app.js
+++ html-css/hc06_js-enter/app.js
@@ -21,9 +21,37 @@ console.log(typeof sayı1);
 // const deneme ;
 // console.log(deneme);
 
-const number1 = 5;
-number1 = 7;
+// const number1 = 5;
+// number1 = 7;
 // ! const ile tanımlanan değişkene başlangıç değişkeni verilmelidir.
-
+//? ======================  LET  ========================
+//* LET de CONST gibi yaygın kullanılan değişken tanımlama yöntemidir.
+//* CONST'dan farkı, sadece tanımlama kısmında değil istenildiği zaman değeri değiştirilebilir.
+//* CONST gibi tanımlandığı blok içerisinde geçerlidir. Başka yerlerden erişilemez. (Block-Scoped)
+//* CONST kullanamadığımız durumlarda (değişkenin değeri sürekli değişecekse) LET kullanmalıyız.
 const varMı = true;
 const isOpen = false;
+console.log(isOpen, typeof isOpen);
+let language = "JAVA";
+console.log(language, typeof language);
+
+language = "JAVASCRİPT";
+console.log(language);
+
+// language = true;
+// console.log(language);
+
+// language = 3.14;
+// console.log(language);
+
+// let adet;
+// console.log(adet);
+
+// adet = "23";
+// console.log(typeof adet);
+// {
+//   let lacalDegişken = 5;
+//   lacalDegişken = lacalDegişken + 1;
+//   console.log(lacalDegişken);
+// }
+// console.log(lacalDegişken);
