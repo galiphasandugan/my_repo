35d9ce4815b15735ae1a6a93caa0d5506eded8f3
galiphasandugan <galiphasandugan@gmail.com>
2023-05-13 13:30:21 +0300

    expression added

--- html-css/hc12_function/app.js
+++ html-css/hc12_function/app.js
@@ -1,10 +1,38 @@
-function yazdirMerhaba() {
-  console.log("Merhaba FS14");
-}
-yazdirMerhaba(name);
+// function yazdirMerhaba() {
+//   console.log("Merhaba FS14");
+// }
+// yazdirMerhaba(name);
 
-function yazdirParametre(name1) {
-  console.log("Merhaba", name1);
-}
-yazdirParemetre("FS14");
-yazdirParemetre("FS15");
+// function yazdirParametre(message, name) {
+//   console.log(message, name);
+// }
+// yazdirParametre("FS14", "FS15");
+// yazdirParametre("Salut", "FS16");
+// yazdirParametre("Hola", "FS17");
+// yazdirParametre("Hallo");
+// yazdirParametre("Ehlen");
+// yazdirParametre();
+
+// function calculate(name, yob) {
+//   const age = new Date().getFullYear() - yob;
+//   console.log(`${name} is ${age} years old`);
+//   return age;
+// }
+// console.log(new Date());
+// calculate("Ahmet", 1990);
+// calculate("İsmet", 1990);
+
+// function isEvenOdd(num) {
+//   if (num % 2 === 0) {
+//     return "EVEN";
+//   } else {
+//     return "ODD";
+//   }
+// }
+// const num = prompt("please enter a number");
+// console.log(`${num} is ${isEvenOdd(num)}`);
+
+function isEvenOdd(num) {
+ return num%2 === 0 ? "EVEN" :"ODD"
+const num = prompt("please enter a number");
+console.log(`${num} is ${isEvenOdd(num)}`);
