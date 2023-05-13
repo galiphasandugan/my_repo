b762a80e06606bf44183de91965b6136c2f5ffa1
galiphasandugan <galiphasandugan@gmail.com>
2023-05-13 12:18:03 +0300

    change

--- html-css/hc11_loops/app.js
+++ html-css/hc11_loops/app.js
@@ -1,22 +1,36 @@
-
-
-question: 
-
-
-const sayı = Number(prompt("sayı girinz"));
-n = 0;
-for (let i = 0; i < sayı; i++) {
-  n += i;
-}
-console.log(n);
-
-
+// const sayı = Number(prompt("sayı girinz"));
+// n = 0;
+// for (let i = 0; i < sayı; i++) {
+//   n += i;
+// }
+// console.log(n);
 
 // console.log(`0 dan ${sayı} kadar olan sayıların toplamı : ${n}`);
 
-
-***************
-
 //** 0-100 arasında 10 adet rastgele sayı üreten kodu for döngüsü ile yazınız.
 
+// const number = prompt("How many numbers:?");
+// //! Math.random()   => 0-1 arasında bir sayı üretir.
 
+// for (let sayac = 1; sayac <= number; sayac++) {
+//   const rastgeleSayı = Math.round(Math.random() * 100);
+//   console.log(rastgeleSayı);
+//   console.log(`${sayac}. sayınız :${rastgeleSayı}`);
+// }
+// let i = 0;
+// while (i < 10) {
+//   console.log("merhaba");
+//   i++;
+// }
+// console.log("BYE");
+// let number = prompt("num giriniz");
+
+// while (number < 0 || number > 100) {
+//   console.log("not 0 dan  küçük veya 100 den b üyük olamaz");
+//   const number = prompt("num giriniz");
+// }
+// console.log("notunuz", number);
+
+// do{
+//   let number = prompt("num giriniz");
+// }
---
+++ html-css/hc12_function/app.js
@@ -0,0 +1,10 @@
+function yazdirMerhaba() {
+  console.log("Merhaba FS14");
+}
+yazdirMerhaba(name);
+
+function yazdirParametre(name1) {
+  console.log("Merhaba", name1);
+}
+yazdirParemetre("FS14");
+yazdirParemetre("FS15");
