05963c50df05730e80eb206a99527d3106756033
galiphasandugan <galiphasandugan@gmail.com>
2023-05-14 14:05:33 +0300

    commit

---
+++ html-css/hc13_contionals/app.js
@@ -0,0 +1,8 @@
+// console.log("hello")
+
+const grade = +prompt("please enter your number");
+// !tek durumlu koşullandırmada kullnaılmak zorundadır
+// grade >= 50 ? console.log("succeed") : console.log("");
+
+const sayı >= 50 && "aferin";
+console.log(sayı);
