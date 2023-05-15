8e16543cd599b4813ff29e3066dd287c133705f9
galiphasandugan <galiphasandugan@gmail.com>
2023-05-15 21:45:06 +0300

    commit

---
+++ html-css/hc14_fonksion2/app.js
@@ -0,0 +1,51 @@
+
+
+// const calculateAge = (year) => new Date().getFullYear() -year
+
+// const r = +prompt("yarıcapı giriniz")
+// const h = +prompt("yuksekliği giriniz")
+// const sHacmi =(r,h)=> Math.PI * r**2 * h 
+
+// console.log(Math.round(sHacmi(r,3)));
+// console.log("volume", sHacmi(r,h).toFixed(2));
+// console.log("volume", sHacmi(r,h).toPrecision(4));
+// console.log("volume", sHacmi(r,h).toExponential(4));
+
+//**toprecision toplam satır sayısını belirtir  */
+// ! expoonentinal  virgülden sonraki miktarı verir
+
+
+
+// const add = (n1,n2) => n1 + n2
+// const extract = (n1,n2) => n1 - n2
+// const multipy = (n1,n2) => n1 * n2
+// const divide = (n1,n2) => n1 / n2
+
+// const hesapMakinesi = (n1,n2,operator) =>{
+//     let result = 0
+//    switch (operator) {
+//     case  "+":
+//         result = add(n1,n2)
+//         break;
+//     case "-":
+//         result = extract(n1,n2)
+//         break;
+//     case "*":
+//         result = multipy(n1,n2)
+//         break;
+//     case "/":
+//         result = divide(n1,n2)
+//         break;
+   
+//     default:
+//         break;
+//    }
+//    return result ;
+// }
+
+// const n1 = +prompt("enter your number")
+// const n2 = +prompt("enter your number")
+// const operator = prompt("enter your operator")
+
+
+// console.log(hesapMakinesi(n1,n2,operator));
