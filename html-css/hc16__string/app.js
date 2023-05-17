0a198c5708afaa5277601d05ff4e5ccf898077cf
galiphasandugan <galiphasandugan@gmail.com>
2023-05-17 17:40:40 +0300

    strings

---
+++ html-css/hc16__string/app.js
@@ -0,0 +1,25 @@
+let str = "hello";
+let str2 = "world";
+let length = str.length;
+
+
+// console.log(length);
+// console.log(str.charAt(0));
+// console.log(str.concat(str2).charAt(8));
+// console.log(str.concat(str2).charAt(8).length);
+
+// console.log(str2.includes("w"));
+// console.log(str.includes("W"));
+
+// console.log(str2.indexOf("w"));
+// console.log(str2.lastIndexOf("r"));
+
+
+
+// console.log(str2.search("l"));
+
+// console.log(str.replace("ali","he"));
+// console.log(str.replace("ali","he"));
+
+
+
