ffa7308bc51d078af6f82f8df554217a2e4d3c74
galiphasandugan <galiphasandugan@gmail.com>
2023-05-18 21:36:16 +0300

    media query

--- html-css/hc16__string/app.js
+++ html-css/hc16__string/app.js
@@ -4,7 +4,7 @@ let length = str.length;
 
 
 // console.log(length);
-// console.log(str.charAt(0));
+console.log(str.charAt(-));
 // console.log(str.concat(str2).charAt(8));
 // console.log(str.concat(str2).charAt(8).length);
 
@@ -21,5 +21,56 @@ let length = str.length;
 // console.log(str.replace("ali","he"));
 // console.log(str.replace("ali","he"));
 
+// let str = "This is a string"
+// let str1 = "This is a string"
+// let str2 = "This is a string"
+// let str4 = new String("This is a string") 
+
+// console.log(str);
+// console.log(str4);
+// console.log(str.length);
+// console.log(str[0]);
+// console.log(str[0]);
 
 
+// const str ="Hello World"
+// // console.log(str.length);
+// // console.log(str[6]);
+
+// for(let i =str.length-1;  i>=0; i--){
+//     console.log(str[i]);
+// }
+
+// let str = `I \'m \'a strinhg `
+// let str1= `I \'m \'a st \trinhg `
+// console.log(str1);
+
+
+// let str = "helllo how are you brother"
+
+// let str2 = str.slice(4,6)
+// console.log(str.slice(4));
+// console.log(str.slice(4,6));
+// console.log(str.slice(4,-2));
+// console.log(str2);
+// console.log(str.substring(0,4));
+// console.log(str.substring(4,0));
+// console.log(str.substring(4,0));
+// console.log(str.substring(2)); 
+// //! hata verirr içerisi eksi değer almaz
+
+// let great = "hello";
+// console.log(object);
+// let str1 = "clarusway";
+// console.log(str1.includes("l"));
+// console.log(str1.includes("l",2));
+// console.log(str1.indexOf("a"));
+// console.log(str1.indexOf("a",3));
+// console.log(str1.indexOf("l",3));
+
+// let email = prompt("email adresini giriniz")
+// console.log(email.includes("@"));
+// console.log(str.includes("@"));
+
+
+ 
